27f986fcae910b807c7bb2a20c3ac8e8c0b6cc05
DhemesonSiqueira <dhemeson_loran@hotmail.com>
2023-07-20 23:49:32 -0300

    feat: list transactions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './module/users/users.module';
+import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
-import { AuthModule } from './module/auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from './module/auth/auth.guard';
-import { CategoriesModule } from './module/categories/categories.module';
-import { BankAccountsModule } from './module/bank-accounts/bank-accounts.module';
+import { AuthGuard } from './modules/auth/auth.guard';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { BankAccountsModule } from './modules/bank-accounts/bank-accounts.module';
+import { TransactionsModule } from './modules/transactions/transactions.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule, BankAccountsModule],
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    AuthModule,
+    CategoriesModule,
+    BankAccountsModule,
+    TransactionsModule,
+  ],
   controllers: [],
   providers: [
     {
