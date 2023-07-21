0647a07fea16888b54f49884d01ac8a76cc59867
DhemesonSiqueira <dhemeson_loran@hotmail.com>
2023-07-20 22:54:48 -0300

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { DatabaseModule } from './shared/database/database.module';
 import { AuthModule } from './module/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './module/auth/auth.guard';
+import { CategoriesModule } from './module/categories/categories.module';
+import { BankAccountsModule } from './module/bank-accounts/bank-accounts.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule],
+  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule, BankAccountsModule],
   controllers: [],
   providers: [
     {
